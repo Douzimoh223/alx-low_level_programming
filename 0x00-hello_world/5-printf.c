16fef83da3d3816e39eaf90516c970d8acb9a4e4
douzimoh223 <momokiing64@gmail.com>
2023-02-16 17:09:39 -0800

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Succes)
+*/
+
+int main(void)
+{
+printfs("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
