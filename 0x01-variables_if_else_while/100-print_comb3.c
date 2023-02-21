cad4c309bb53fc27634196fa5d24556f38e500bd
douzimoh223 <momokiing64@gmail.com>
2023-02-21 08:04:45 -0800

    100base1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
-*main - Prints all possible different combinations of two digits
-*Return: Always 0 (Succes)
-*/
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
 int main(void)
 {
 int n, m;
 for (n = 48; n <= 56; n++)
 {
-for (n = 49; n <= 57; m++)
+for (m = 49; m <= 57; m++)
 {
 if (m > n)
 {
@@ -25,3 +25,4 @@ putchar(' ');
 putchar('\n');
 return (0);
 }
+
