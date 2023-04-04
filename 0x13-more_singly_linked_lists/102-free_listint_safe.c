7dc40e2c8bf04327e501609206309d1a3b35fc81
douzimoh223 <momokiing64@gmail.com>
2023-04-03 17:12:01 -0700

    longlist

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,26 +1,35 @@
 #include "lists.h"
 /**
- * free_listint_safe - Frees a listint_t linked list
- * @h: Pointer to a pointer to the head node of the list
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
  *
- * Return: The size of the list that was freed
+ * Return: number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
-size_t size = 0;
-listint_t *tmp, *prev = NULL;
-if (h == NULL || *h == NULL)
+size_t len = 0;
+int diff;
+listint_t *temp;
+if (!h || !*h)
 return (0);
-while (*h != NULL)
+while (*h)
 {
-size++;
-tmp = *h;
-*h = (*h)->next;
-if (tmp < prev)
+diff = *h - (*h)->next;
+if (diff > 0)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+len++;
+}
+else
+{
+free(*h);
+*h = NULL;
+len++;
 break;
-prev = tmp;
-free(tmp);
+}
 }
 *h = NULL;
-return (size);
+return (len);
 }
