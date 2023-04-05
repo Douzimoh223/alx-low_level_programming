37457bee251e1e33e0651d06bcb1e6045ecddb28
douzimoh223 <momokiing64@gmail.com>
2023-04-05 16:20:59 -0700

    indexfree

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 /**
  * listint_len - Returns the number of elements in a linked listint_t list.
  *  @h:pointer number linked listint_t list.
- i* Return: number of elements in h
+ * Return: number of elements in h
  */
 size_t listint_len(const listint_t *h)
 {
--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,8 +1,10 @@
-#include "lists.h"
+#include "lists"
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - deletes the node at
+ * index index of a listint_t linked list
  * @head: pointer to pointer to the head node of the list
  * @index: index of the node that should be deleted
+ *
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
