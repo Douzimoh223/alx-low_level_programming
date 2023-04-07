36962e257bc88b430ad3e1eba00ecf7b68a3b161
douzimoh223 <momokiing64@gmail.com>
2023-04-07 16:55:09 -0700

    bintwo

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,12 +3,13 @@
  * print_binary -  a function that prints
  * the binary representation of a number
  * @n :pointer of print the binary
+ * Return: result
  */
 void print_binary(unsigned long int n)
 {
 if (n > 1)
 {
-print_binary(n >> 1);
+print_binary(n  >> 1);
 }
-printf("%lu", n & 1);
+return void(print_binary(unsigned long int n));
 }
